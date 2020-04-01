70895bd2925202e7582bc36f438cd7919ccd4d45
yangc999 <yangc999@163.com>
2020-04-01 17:38:43 +0800

    visit

--- add_prefix.rb
+++ add_prefix.rb
@@ -2,29 +2,30 @@
 require 'xcodeproj'
 require 'pry'
 
+def visit(children)
+    children.each do |child|
+        if child.class == Xcodeproj::Project::Object::PBXGroup
+            puts 'group'
+            puts child.display_name
+            puts child.path
+            visit(child.children)
+        elsif child.class == Xcodeproj::Project::Object::PBXFileReference
+            puts 'file'
+            puts child.display_name
+            puts child.path            
+        end
+    end
+end
+
 puts 'input xcodeproj file path:'
 proj_path = gets.chomp
 #proj_path = '/home/yangc/testconfuse/com.test.confuse/frameworks/runtime-src/proj.ios_mac/com.test.confuse.xcodeproj'
 if File.exists?(proj_path) and File.directory?(proj_path)
     proj_dir = File.dirname(proj_path)
     proj = Xcodeproj::Project.open(proj_path)
-    puts proj_dir
+    puts proj.main_group.path
+    visit(proj.main_group.children)
     proj.files.each do |file|
         ext = File.extname(file.path)
-        if ext == '.h' or ext == '.hpp'
-            puts 'name:' + file.display_name
-            puts 'path:' + file.path
-            puts 'parent:' + file.parent.display_name
-        end
-        if ext == '.m' or ext == '.mm'
-            puts 'name:' + file.display_name
-            puts 'path:' + file.path
-            puts 'parent:' + file.parent.display_name
-        end
-        if ext == '.c' or ext == '.cpp'
-            puts 'name:' + file.display_name
-            puts 'path:' + file.path
-            puts 'parent:' + file.parent.display_name
-        end
     end
 end
