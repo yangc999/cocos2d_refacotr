56d785ba1ea536a4949c015011c3a80c737009b0
yangc999 <yangc999@163.com>
2020-04-02 17:14:24 +0800

    stub

--- add_stub.rb
+++ add_stub.rb
@@ -1,9 +1,21 @@
 
 require 'xcodeproj'
+require 'pathname'
 
-proj = nil
+$proj = nil
+$proj_path = '/home/yangc/testconfuse/com.test.confuse/frameworks/runtime-src/proj.ios_mac/com.test.confuse.xcodeproj'
+
+$proj = nil
 puts 'input xcodeproj file path:'
-proj_path = gets.chomp
-if File.exists?(proj_path) and File.directory?(proj_path)
-    proj = Xcodeproj::Project.open(proj_path)
+path = gets.chomp
+$proj_path = path.length > 0 ? path : $proj_path
+if File.exists?($proj_path) and File.directory?($proj_path)
+    $proj = Xcodeproj::Project.open($proj_path)
+    dst_path = $proj.main_group.real_path.to_s
+    src_path = Pathname.new(File.dirname(__FILE__)).realpath
+    src = '%s/stub.h' % src_path
+    dst = '%s/stub.h' % dst_path
+    FileUtils.cp(src, dst)
+    $proj.main_group.new_reference(dst)
+    $proj.save()
 end
