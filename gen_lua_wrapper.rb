da5b6be132d799d1b28a37904db7a811d932ebee
yangc999 <yangc999@163.com>
2020-04-03 15:58:20 +0800

    fix yaml file name

--- gen_lua_wrapper.rb
+++ gen_lua_wrapper.rb
@@ -91,7 +91,7 @@ path = gets.chomp
 $proj_path = path.length > 0 ? path : $proj_path
 if File.exists?($proj_path) and File.directory?($proj_path)
     $proj = Xcodeproj::Project.open($proj_path)
-    yaml_path = '%s/lua_todo.yaml' % $proj.main_group.real_path.to_s 
+    yaml_path = '%s/lua.yaml' % $proj.main_group.real_path.to_s 
     if File.exists?(yaml_path)
         yaml = Yaml.load(File.open(yaml_path))
         gen_lua(yaml)
